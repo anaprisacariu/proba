d00d833338cc7d8dd91371a644546779a359d4db
prisa <ana-maria.prisacariu@student.tuiasi.ro>
2023-04-21 12:19:47 +0300

    added salut project

---
+++ Salut/src/main/java/mashina/Salut/App.java
@@ -0,0 +1,13 @@
+package mashina.Salut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
